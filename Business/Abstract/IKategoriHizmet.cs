3d4780cf1a812815d812d070071ba1f17f166fea
osmanucar <63417248+osmanucar@users.noreply.github.com>
2021-03-24 00:50:42 +0300

    Business (iş) katmanı hazırlandı

---
+++ Business/Abstract/IKategoriHizmet.cs
@@ -0,0 +1,18 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IKategoriHizmet
+    {
+        List<Kategoriler> GetAll();
+
+        void Create(Kategoriler entity);
+
+        void Update(Kategoriler entity);
+
+        void Delete(Kategoriler entity);
+    }
+}
---
+++ Business/Abstract/IUrunHizmet.cs
@@ -0,0 +1,21 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IUrunHizmet
+    {
+        Urunler GetById(int id);
+
+        List<Urunler> GetAll(); // bütün ürünleri getirecek bir metot
+
+        void Create(Urunler entity);
+
+        void Update(Urunler entity);
+
+        void Delete(Urunler entity); 
+        
+    }
+}
