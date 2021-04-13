ba0d2fe2de19437cc0c39ef0e245d2408fdf937f
osmanucar <63417248+osmanucar@users.noreply.github.com>
2021-04-13 04:07:05 +0300

    Kullanıcı Sayfası Hazırlandı

--- TeknolojikAletSatisSitesi/TeknolojikAletSatisSitesi.Entities/Product.cs
+++ TeknolojikAletSatisSitesi/TeknolojikAletSatisSitesi.Entities/Product.cs
@@ -9,6 +9,7 @@ namespace TeknolojikAletSatisSitesi.Entities
         public int Id { get; set; }
         public String Name { get; set; }
         public String ImageUrl { get; set; }
+        public string Description { get; set; }
         public decimal Price { get; set; }
 
         public List<ProductCategory> ProductCategories { get; set; }
