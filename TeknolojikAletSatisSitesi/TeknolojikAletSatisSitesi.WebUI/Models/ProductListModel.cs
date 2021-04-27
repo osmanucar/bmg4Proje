74f647f3fe7832f7eebc9612612c7d7282905dbc
Volkanknc <63414550+Volkanknc@users.noreply.github.com>
2021-04-27 07:00:12 +0300

    Validation

---
+++ TeknolojikAletSatisSitesi/TeknolojikAletSatisSitesi.WebUI/Models/ProductListModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TeknolojikAletSatisSitesi.Entities;
+
+namespace TeknolojikAletSatisSitesi.WebUI.Models
+{
+    public class PageInfo
+    {
+        public int TotalItems { get; set; } // toplam ürün sayısı
+        public int ItemsPerPage { get; set; } // her sayfada kaç ürün göstermek istiyoruz
+        public int CurrentPage { get; set; } // o anki sayfa
+        public string CurrentCategory { get; set; } // o anki kategori
+
+        public int TotalPages()
+        {
+            return (int)Math.Ceiling((decimal)TotalItems/ItemsPerPage);
+        }
+    }
+    public class ProductListModel
+    {
+        public PageInfo PageInfo { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
