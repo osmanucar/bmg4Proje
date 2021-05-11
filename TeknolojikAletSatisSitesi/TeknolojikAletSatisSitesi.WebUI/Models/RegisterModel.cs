b29d4205785a580ab80b4935d0e274b759951e5d
Volkanknc <63414550+Volkanknc@users.noreply.github.com>
2021-05-11 06:20:47 +0300

    Üyelik İşlemleri

---
+++ TeknolojikAletSatisSitesi/TeknolojikAletSatisSitesi.WebUI/Models/RegisterModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeknolojikAletSatisSitesi.WebUI.Models
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "Ad Soyad alanı zorunludur.")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Kullanıcı adı alanı zorunludur.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Parola alanı zorunludur.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Tekrar parola alanı zorunludur.")]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string RePassword { get; set; }
+
+        [Required(ErrorMessage = "E-posta alanı zorunludur.")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+}
