283e6e909c8a547a2818b3d34c4745e350d1aadc
Volkanknc <63414550+Volkanknc@users.noreply.github.com>
2021-05-11 06:19:37 +0300

    Üyelik İşlemleri

---
+++ TeknolojikAletSatisSitesi/TeknolojikAletSatisSitesi.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TeknolojikAletSatisSitesi.WebUI.Identity;
+using TeknolojikAletSatisSitesi.WebUI.Models;
+
+namespace TeknolojikAletSatisSitesi.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public IActionResult Register()
+        {
+            return View(new RegisterModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = new ApplicationUser
+            {
+                UserName = model.Username,
+                Email = model.Email,
+                FullName = model.FullName
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                // generate token
+                // send email
+
+                return RedirectToAction("account", "login");
+            }
+
+
+            ModelState.AddModelError("", "Bilinmeyen hata oluştu lütfen tekrar deneyiniz.");
+            return View(model);
+        }
+    }
+}
--- TeknolojikAletSatisSitesi/TeknolojikAletSatisSitesi.WebUI/Controllers/AdminController.cs
+++ TeknolojikAletSatisSitesi/TeknolojikAletSatisSitesi.WebUI/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,7 @@ using TeknolojikAletSatisSitesi.WebUI.Models;
 
 namespace TeknolojikAletSatisSitesi.WebUI.Controllers
 {
+    [Authorize]
     public class AdminController : Controller
     {
         private IProductService _productService;
