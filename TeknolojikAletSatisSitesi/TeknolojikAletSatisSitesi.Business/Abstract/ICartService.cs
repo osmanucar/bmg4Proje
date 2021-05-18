02717cfa3ba080a82c1113f5f699b786621546a2
osmanucar <63417248+osmanucar@users.noreply.github.com>
2021-05-18 15:47:43 +0300

    Kullanıcı Yetkilendirme ve Alışveriş Sepeti

--- TeknolojikAletSatisSitesi/TeknolojikAletSatisSitesi.Business/Abstract/ICartService.cs
+++ TeknolojikAletSatisSitesi/TeknolojikAletSatisSitesi.Business/Abstract/ICartService.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TeknolojikAletSatisSitesi.Entities;
 
 namespace TeknolojikAletSatisSitesi.Business.Abstract
 {
     public interface ICartService
     {
         void InitializeCart(string userId);
+        Cart GetCartByUserId(string userId);
     }
 }
