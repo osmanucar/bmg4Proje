a6c4ab5faa313e0341b488451adc7aaccf56e516
osmanucar <63417248+osmanucar@users.noreply.github.com>
2021-05-18 15:49:56 +0300

    Kullanıcı Yetkilendirme ve Alışveriş Sepeti

--- TeknolojikAletSatisSitesi/TeknolojikAletSatisSitesi.DataAccess/Concrete/EfCore/EfCoreCartDal.cs
+++ TeknolojikAletSatisSitesi/TeknolojikAletSatisSitesi.DataAccess/Concrete/EfCore/EfCoreCartDal.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using TeknolojikAletSatisSitesi.DataAccess.Abstract;
 using TeknolojikAletSatisSitesi.Entities;
@@ -8,5 +10,16 @@ namespace TeknolojikAletSatisSitesi.DataAccess.Concrete.EfCore
 {
     public class EfCoreCartDal : EfCoreGenericRepository<Cart, ShopContext>, ICartDal
     {
+        public Cart GetByUserId(string userId)
+        {
+            using(var context = new ShopContext())
+            {
+                return context
+                            .Carts
+                            .Include(i => i.CartItems)
+                            .ThenInclude(i => i.Product)
+                            .FirstOrDefault(i => i.UserId == userId);
+            }
+        }
     }
 }
