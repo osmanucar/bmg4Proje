f80a1e48d5c49aed31d241f208f78ff48e09f0af
Volkanknc <63414550+Volkanknc@users.noreply.github.com>
2021-05-18 12:57:20 +0300

    Kullanıcı Yetkilendirme ve Alışveriş Sepeti

---
+++ TeknolojikAletSatisSitesi/TeknolojikAletSatisSitesi.Business/Abstract/ICartService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeknolojikAletSatisSitesi.Business.Abstract
+{
+    public interface ICartService
+    {
+        void InitializeCart(string userId);
+    }
+}
---
+++ TeknolojikAletSatisSitesi/TeknolojikAletSatisSitesi.Business/Concrete/CartManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TeknolojikAletSatisSitesi.Business.Abstract;
+using TeknolojikAletSatisSitesi.DataAccess.Abstract;
+using TeknolojikAletSatisSitesi.Entities;
+
+namespace TeknolojikAletSatisSitesi.Business.Concrete
+{
+    public class CartManager : ICartService
+    {
+        private ICartDal _cartDal;
+        public CartManager(ICartDal cartDal)
+        {
+            _cartDal = cartDal;
+        }
+        public void InitializeCart(string userId)
+        {
+            _cartDal.Create(new Cart() { UserId = userId });
+        }
+    }
+}
