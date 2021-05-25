db64be7f09feaaae706591d418c0ea95698e628d
Volkanknc <63414550+Volkanknc@users.noreply.github.com>
2021-05-25 03:26:39 +0300

    Sipariş ve Ödeme İşlemleri

--- TeknolojikAletSatisSitesi/TeknolojikAletSatisSitesi.Business/Abstract/ICartService.cs
+++ TeknolojikAletSatisSitesi/TeknolojikAletSatisSitesi.Business/Abstract/ICartService.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TeknolojikAletSatisSitesi.Entities;
 
 namespace TeknolojikAletSatisSitesi.Business.Abstract
 {
     public interface ICartService
     {
         void InitializeCart(string userId);
+        Cart GetCartByUserId(string userId);
+        void AddToCart(string userId, int productId, int quantity);
+        void DeleteFromCart(string userId, int productId);
     }
 }
